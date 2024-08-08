a912f1b41d633c8c9e1d6834855ff5fac57f87d9
Mateus Lopes <mateusdevtools@gmail.com>
2024-08-08 14:45:31 -0300

    feat: refresh token use case

---
+++ src/data/config/auth.ts
@@ -0,0 +1,9 @@
+const auth = {
+  expires_in_token: "15m",
+  expires_in_refresh_token: "30d",
+  refresh_token_expires_days: 30,
+  secret_token: "c91c03ea6c46a86cbc019be3d71d0a1a",
+  secret_refresh_token: "067f4e310d6b21358dc0886317c77deb"
+}
+
+export { auth };
---
+++ src/domain/contexts/auth/useCases/refreshToken/RefreshTokenUseCase.ts
@@ -0,0 +1,70 @@
+
+import { inject, injectable } from "tsyringe";
+import { sign, verify } from "jsonwebtoken"
+import dayjs from "dayjs";
+import { AppError } from "../../../../../shared/errors/AppError";
+import { auth } from "../../../../../data/config/auth";
+import { IUsersTokensRepository } from "../../repositories/contracts/IUsersTokensRepository";
+
+interface IPayload {
+  subject: string;
+  email: string;
+}
+
+interface ITokenResponse {
+  token: string;
+  refreshToken: string;
+}
+
+@injectable()
+class RefreshTokenUseCase {
+
+  constructor(
+    @inject("UsersTokensRepository")
+    private usersTokensRepository: IUsersTokensRepository
+  ) { }
+
+  async execute(refreshToken: string) {
+    const { secret_refresh_token, refresh_token_expires_days, secret_token } = auth
+
+    const { subject } = verify(refreshToken, secret_refresh_token) as IPayload
+    const userId = subject
+
+    const userToken = await this.usersTokensRepository.findByrefreshToken(refreshToken)
+
+    if (!userToken) {
+      throw new AppError("ERR_REFRESH_TOKEN_NOT_FOUND")
+    }
+
+    await userToken.destroy()
+
+    const newRefreshToken = sign(
+      {
+        subject,
+        expiresIn: auth.expires_in_refresh_token
+      },
+      secret_refresh_token,
+      {}
+    )
+
+    const expiresDate = dayjs().add(refresh_token_expires_days, "days").toDate()
+
+    await this.usersTokensRepository.create({
+      expiresDate,
+      refreshToken: newRefreshToken,
+      userId: +userId
+    })
+
+    const newToken = sign({
+      subject: userId,
+      expiresIn: auth.expires_in_token
+    }, secret_token, {}
+    );
+
+    return {
+      refreshToken,
+      token: newToken
+    }
+  }
+}
+export { RefreshTokenUseCase }
