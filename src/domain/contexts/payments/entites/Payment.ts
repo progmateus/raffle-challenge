72b99b01ab8a9f67bf5690b6708a6b5096eecaca
Mateus Lopes <mateusdevtools@gmail.com>
2024-08-09 03:22:42 -0300

    feat: payment entity

---
+++ src/domain/contexts/payments/entites/Payment.ts
@@ -0,0 +1,59 @@
+import { AutoIncrement, Column, CreatedAt, Model, PrimaryKey, Table, UpdatedAt } from "sequelize-typescript";
+
+@Table
+class Payment extends Model {
+
+  @PrimaryKey
+  @AutoIncrement
+  @Column
+  id: number;
+
+  @Column
+  method: string;
+
+  @Column
+  recurrence: boolean;
+
+  @Column
+  installments: number;
+
+  @Column
+  statementDescriptor: string;
+
+  @Column
+  cardFinalNumbers: string;
+
+  @Column
+  cardHolderName: string;
+
+  @Column
+  cardExpMonth: number;
+
+  @Column
+  cardExpYear: number;
+
+  @Column
+  cardCvv: string;
+
+  @Column
+  line1: string;
+
+  @Column
+  zipCode: string;
+
+  @Column
+  city: string;
+
+  @Column
+  state: string;
+
+  @Column
+  country: string;
+
+  @CreatedAt
+  createdAt: Date;
+
+  @UpdatedAt
+  updatedAt: Date;
+}
+export { Payment }
