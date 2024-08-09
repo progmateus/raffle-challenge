b1234213f146720966fa71f35c6d30adf354afe5
Mateus Lopes <mateusdevtools@gmail.com>
2024-08-08 22:55:17 -0300

    feat: number entity

---
+++ src/domain/contexts/numbers/entites/Number.ts
@@ -0,0 +1,27 @@
+import { AutoIncrement, BelongsTo, Column, CreatedAt, ForeignKey, Model, PrimaryKey, Table, UpdatedAt } from "sequelize-typescript";
+import { User } from "../../accounts/entities/User";
+
+@Table
+class Number extends Model {
+  @PrimaryKey
+  @AutoIncrement
+  @Column
+  id: number;
+
+  @ForeignKey(() => User)
+  @Column
+  userId: number;
+
+  @BelongsTo(() => User)
+  user: User;
+
+  @Column
+  number: string;
+
+  @CreatedAt
+  createdAt: Date;
+
+  @UpdatedAt
+  updatedAt: Date;
+}
+export { Number }
