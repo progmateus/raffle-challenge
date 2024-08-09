0e3accd911fb0b56398753cd9dada9d136bba2ff
Mateus Lopes <mateusdevtools@gmail.com>
2024-08-09 16:12:50 -0300

    users migrations

---
+++ src/data/migrations/20240809191156-create-users.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const { DataTypes } = require('sequelize');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    return queryInterface.createTable('Users', {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      }
+    })
+  },
+
+  async down(queryInterface, Sequelize) {
+    return queryInterface.dropTable('Users')
+  }
+};
