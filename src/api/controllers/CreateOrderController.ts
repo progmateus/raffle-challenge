20c9dc95533cb7babc9b2c4a8c431a8678543292
Mateus Lopes <mateusdevtools@gmail.com>
2024-08-09 16:27:34 -0300

    fix: create order controller

--- src/api/controllers/CreateOrderController.ts
+++ src/api/controllers/CreateOrderController.ts
@@ -2,10 +2,11 @@ import { Request, Response } from "express";
 import { container } from "tsyringe";
 import { CreateUserUseCase } from "../../domain/contexts/accounts/useCases/createUser/CreateUserUseCase";
 import { CreateOrderUseCase } from "../../domain/contexts/orders/useCases/CreateOrderUseCase";
+import { ICreateOrderDTO } from "../../domain/contexts/orders/dtos/ICreateOrderDTO";
 
 class CreateOrderController {
   async handle(request: Request, response: Response) {
-    const { payment, qtdNumbers } = request.body;
+    const { payment, qtdNumbers } = request.body as ICreateOrderDTO;
     const { id } = request.user;
     const createOrderUseCase = container.resolve(CreateOrderUseCase)
     await createOrderUseCase.execute({ payment, qtdNumbers, userId: id });
