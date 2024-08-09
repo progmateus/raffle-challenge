598369aeb259e056c42ae040618bc096cb4e036c
Mateus Lopes <mateusdevtools@gmail.com>
2024-08-09 13:06:38 -0300

    create payment use case

--- src/domain/contexts/payments/useCases/createPayment/CreatePaymentUsecase.ts
+++ src/domain/contexts/payments/useCases/createPayment/CreatePaymentUsecase.ts
@@ -2,7 +2,30 @@ import { inject, injectable } from "tsyringe";
 import { ICreatePaymentDTO } from "../../dtos/ICreatePaymentDTO";
 import { Payment } from "../../entites/Payment";
 import { IPaymentsRepository } from "../../repositories/contracts/IPaymentsRepository";
+import { z } from "zod";
 
+const paymentSchema = z.object({
+  payment_method: z.string(),
+  credit_card: z.object({
+    recurrence: z.boolean(),
+    installments: z.number().positive(),
+    statement_descriptor: z.string(),
+    card: z.object({
+      number: z.string().length(16),
+      holder_name: z.string(),
+      exp_month: z.number().positive(),
+      exp_year: z.number().positive(),
+      cvv: z.string().length(3),
+      billing_address: z.object({
+        line_1: z.string(),
+        zip_code: z.string(),
+        cit: z.string(),
+        state: z.string(),
+        country: z.string()
+      })
+    })
+  })
+});
 @injectable()
 class CreatePaymentUsecase {
   constructor(
@@ -11,6 +34,8 @@ class CreatePaymentUsecase {
   ) { }
 
   async execute({ payment_method, credit_card }: ICreatePaymentDTO): Promise<Payment> {
+
+    paymentSchema.parse({ payment_method, credit_card })
 
     return await this.PaymentsRepository.create({
       credit_card,
