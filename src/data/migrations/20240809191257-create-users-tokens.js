e9c2af3d794756bcb510fa4187f9174bd44a0c9b
Mateus Lopes <mateusdevtools@gmail.com>
2024-08-09 16:13:18 -0300

    users tokens migrations

---
+++ src/data/migrations/20240809191257-create-users-tokens.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const { DataTypes } = require('sequelize');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    return queryInterface.createTable('UsersTokens', {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false
+      },
+      userId: {
+        type: DataTypes.INTEGER,
+        references: { model: "Users", key: "id" },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE"
+      },
+      refreshToken: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      expiresDate: {
+        type: DataTypes.DATE,
+        allowNull: false
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      }
+    })
+  },
+
+  async down(queryInterface, Sequelize) {
+    return queryInterface.dropTable('UsersTokens')
+  }
+};
