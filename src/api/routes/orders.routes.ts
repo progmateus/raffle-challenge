0d5f65cd13af1cf72ccdcb5a8952ad34b7fa46dd
Mateus Lopes <mateusdevtools@gmail.com>
2024-08-09 16:28:28 -0300

    feat: Add middleware order route

--- src/api/routes/orders.routes.ts
+++ src/api/routes/orders.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { CreateOrderController } from "../controllers/CreateOrderController";
+import { ensureAuthenticated } from "../middlewares/EnsureAuthenticated";
 
 const ordersRoutes = Router();
 const createOrderController = new CreateOrderController();
 
 
-ordersRoutes.post("/", createOrderController.handle);
+ordersRoutes.post("/", ensureAuthenticated, createOrderController.handle);
 
 export { ordersRoutes };
